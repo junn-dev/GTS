3d2918ed97444eb589a5fc17c3e9a0512b4befe2
junn-dev <hungnguyen.junn@gmail.com>
2025-06-28 09:50:50 +0700

    Update HinhThang.c

--- HinhThang.c
+++ HinhThang.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-#define f(x) x * pow(M_E, -x)
+//#define f(x) x * pow(M_E, -x)
+#define f(x) (5 * x + 4) / (6 * x * x + 9)
 
 int main() {
     int n;
@@ -26,3 +27,25 @@ int main() {
     return 0;
 }
 
+//#include <stdio.h>
+//
+//int main() {
+//    
+//    double x[] = {1.11, 1.29, 1.47, 1.65, 1.83, 2.01, 2.19};
+//    double y[] = {1.28, 1.66, 2.41, 1.76, 2.63, 3.28}; // thieu A
+//    int n = 6;
+//    double h = 0.18;
+//    double target = 4.2;
+//
+//    double sum = x[0] * y[0];
+//	int i; 
+//    for (i = 1; i < n; i++) {
+//        sum += 2 * x[i] * y[i];
+//    }
+//
+//    double A = (4.2 * 2 / h - sum) / x[6];
+//
+//    printf("Gia tri A = f(%.2lf) can tim la: %.10lf\n", x[6], A);
+//    return 0;
+//}
+//
